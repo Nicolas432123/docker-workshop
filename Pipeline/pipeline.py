c2fb974459f5f06635772e255bad2d9dc735b5c9
Nicolas432123 <nicolas.alvarez.ahumada@gmail.com>
2026-01-16 13:30:56 +0000

    pipeline

---
+++ Pipeline/pipeline.py
@@ -0,0 +1,22 @@
+import sys
+
+import pandas as pd 
+
+
+
+print    ('arguments',sys.argv)
+
+
+
+months= int(sys.argv[1])
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+
+print(df.head())
+
+df.to_parquet(f"output_{months}.parquet")
+
+
+
+
+print    (f'hello pipeline, month={months}')
